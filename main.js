e9b09d3929953dedfdcc2afc26c280e6107498e4
LucasGaligniana <galigniana@hotmail.com>
2022-06-29 13:32:37 -0300

    agrego un evento al boton Bienvenido. Al click muestra un alert

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function holaJulio(){console.log ("hola julio")}
+function holaJulio(){
+    console.log ("hola julio")}
+
+function holaJulio1(){
+        alert ("hola julio")}
+    
 document.getElementById("libienve").onmouseover= holaJulio;
+document.getElementById("libienve").onclick= holaJulio1;
 
